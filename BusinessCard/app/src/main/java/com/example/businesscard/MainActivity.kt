8619916fd062722f5da103541682c0a4e453e7c5
Christopher Høe <christopher.hoee@gmail.com>
2024-07-09 12:27:41 +0200

    feat: finished app

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -21,8 +21,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Phone
@@ -31,9 +34,9 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,8 +47,7 @@ class MainActivity : ComponentActivity() {
                 val hexString = "#3280c4"
                 val intHex = hexString.substring(1).toInt(16)
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color(intHex)
+                    modifier = Modifier.fillMaxSize(), color = Color(intHex)
                 ) {
                     val image = painterResource(R.drawable.android_logo)
                     BusinessCardContainer(
@@ -54,7 +56,8 @@ class MainActivity : ComponentActivity() {
                         title = stringResource(R.string.title),
                         phoneNumber = stringResource(R.string.phone),
                         shareSocial = stringResource(R.string.social),
-                        email = stringResource(R.string.email)
+                        email = stringResource(R.string.email),
+                        color = Color(0xFF3280c4)
                     )
                 }
             }
@@ -69,14 +72,19 @@ fun BusinessCardContainer(
     title: String,
     phoneNumber: String,
     shareSocial: String,
-    email: String
+    email: String,
+    color: Color
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        AboutComponent(image = image, name = name, title = title, modifier = Modifier.weight(1f))
+
+        ) {
+        AboutComponent(image = image, name = name, title = title, modifier = Modifier.weight(2f))
+        Spacer(modifier = Modifier.weight(0.5f))
         ContactComponent(
             phoneNumber = phoneNumber,
             shareSocial = shareSocial,
@@ -98,82 +106,72 @@ private fun AboutComponent(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = image,
-            contentDescription = "Android logo",
-            modifier = modifier.fillMaxWidth()
+            painter = image, contentDescription = "Android logo", modifier = modifier.fillMaxWidth()
 
         )
         Text(
             text = name,
             fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
+            textAlign = TextAlign.Center,
+
+            )
         Text(
-            text = title,
-            textAlign = TextAlign.Justify
+            text = title, textAlign = TextAlign.Justify, color = Color(0xFF66A866)
         )
     }
 }
 
 @Composable
 private fun ContactComponent(
-    phoneNumber: String,
-    shareSocial: String,
-    email: String,
-    modifier: Modifier = Modifier
+    phoneNumber: String, shareSocial: String, email: String, modifier: Modifier = Modifier
 ) {
     Column(
         modifier
             .fillMaxHeight()
-            .padding(10.dp)
+            .padding(10.dp), verticalArrangement = Arrangement.spacedBy(28.dp)
     ) {
-        Row(modifier.fillMaxWidth()) {
-            PhoneIcon(tint = Color.Black)
-            Text(
-                text = phoneNumber
-            )
-        }
-        Row(modifier.fillMaxWidth()) {
-            SocialIcon(tint = Color.Black)
-            Text(
-                text = shareSocial
-            )
-        }
-        Row(modifier.fillMaxWidth()) {
-            EmailIcon(tint = Color.Black)
-            Text(
-                text = email
-            )
-        }
+        CenteredContactRow(icon = { PhoneIcon(tint = Color.Black) }, text = phoneNumber)
+        CenteredContactRow(icon = { SocialIcon(tint = Color.Black) }, text = shareSocial)
+        CenteredContactRow(icon = { EmailIcon(tint = Color.Black) }, text = email)
     }
 
+}
 
+@Composable
+fun CenteredContactRow(
+    icon: @Composable () -> Unit, text: String, modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        icon()
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text, fontSize = 14.sp
+        )
+    }
 }
 
 @Composable
 fun PhoneIcon(tint: Color = LocalContentColor.current) {
     Icon(
-        imageVector = Icons.Rounded.Phone,
-        tint = tint,
-        contentDescription = "Phone icon"
+        imageVector = Icons.Rounded.Phone, tint = tint, contentDescription = "Phone icon"
     )
 }
 
 @Composable
 fun SocialIcon(tint: Color = LocalContentColor.current) {
     Icon(
-        imageVector = Icons.Rounded.Share,
-        tint = tint,
-        contentDescription = "Social share"
+        imageVector = Icons.Rounded.Share, tint = tint, contentDescription = "Social share"
     )
 }
 
 @Composable
 fun EmailIcon(tint: Color = LocalContentColor.current) {
     Icon(
-        imageVector = Icons.Rounded.Email,
-        tint = tint,
-        contentDescription = "Social share"
+        imageVector = Icons.Rounded.Email, tint = tint, contentDescription = "Social share"
     )
 }
 
@@ -188,7 +186,8 @@ fun BusinessCardPreview() {
             title = stringResource(R.string.title),
             phoneNumber = stringResource(R.string.phone),
             shareSocial = stringResource(R.string.social),
-            email = stringResource(R.string.email)
+            email = stringResource(R.string.email),
+            color = Color(0xFF3280c4)
         )
     }
 }
